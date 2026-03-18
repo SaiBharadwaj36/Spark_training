221cc8938388ee5534230dd6f852d6c050b6741c
AS-MAC-1328 <as-mac-1328@AS-MAC-1328.local>
2026-03-18 14:36:08 +0530

    last changes by sai

--- Scripts/script-1.py
+++ Scripts/script-1.py
@@ -1,1 +1,33 @@
-print('Starting the project')
+# a='sai'
+# print(dir(a))
+# print(a.split())
+
+# immutable
+# hetro or homogeneous
+# hashable
+# duplicates
+
+# why dictionary is fast and what is O(1)
+# what is a constructor and can we use multiple constructors or not in python
+
+# data={
+#     ('sai',1):30,
+#     ('pranay',2):50
+# }
+# print(data[('sai',1)])
+
+#How and Why I need multiple constructors
+
+# def demo_function(*args):
+#     print(*args)
+
+# demo_function(1,2,3,4)
+
+a=123
+b='sai'
+
+print(f'Id is {a} and name is {b}')
+
+
+
+
